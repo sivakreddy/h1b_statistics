5db1ac2530704d40ea7493346cd82ff8b52c4bf3
Siva <sivakreddy81@gmail.com>
2018-10-29 13:54:12 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-#
-#!python3
-#
-python ./src/h1b_analysis.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
+#!/bin/sh
+
+#!/usr/bin/env python3
+
+python3 ./src/h1b_analysis.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
 
 read  -n 1 -p "Hit any key to exit" mainmenuinput
